450ce0b98182b69d5b1a1abfd6bba6f1139f3be1
Josu Labrador <josulabrador@gmail.com>
2017-01-17 19:38:30 +0100

    Commit 01 Martes 17 - 19:40

---
+++ GMaps v01/GMaps v01/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GMaps v01
+//
+//  Created by  on 17/1/17.
+//  Copyright © 2017 Egibide. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
